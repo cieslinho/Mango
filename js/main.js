ca170e9c728e7e9468c746429d6dfea1d2051e74
Szymon Cieśla <cszymon01@gmail.com>
2022-10-10 10:38:32 +0200

    finished website - mangoclub.pl

---
+++ js/main.js
@@ -0,0 +1,18 @@
+const nav = document.querySelector('.nav__menu')
+const navBtn = document.querySelector('.burger-btn')
+const allNavItems = document.querySelectorAll('.nav__item')
+
+const handleNav = () => {
+	nav.classList.toggle('nav__menu--active')
+	allNavItems.forEach(item => {
+		item.addEventListener('click', () => {
+			nav.classList.remove('nav__menu--active')
+		})
+	})
+}
+
+function popupMail() {
+	alert('Wiadomość została wysłana')
+}
+
+navBtn.addEventListener('click', handleNav)
